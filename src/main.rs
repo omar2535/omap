a3b2b32cd9fe90508d180070420b94b705f55638
Omar Tsai <omar2535@gmail.com>
2020-03-27 00:00:50 -0700

    adding clap to project

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+extern crate clap;
+
+use clap::{Arg, App};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
+    let matches = App::new("Omap")
+                          .version("1.0")
+                          .author("Omar T. <omar2535@alumni.ubc.ca>")
+                          .about("Scans open ports at specified address")
+                          .arg(Arg::with_name("address")
+                              .short("-a")
+                              .long("address")
+                              .help("Sets the address to be scanned")
+                              .takes_value(true))
+                          .get_matches();
     println!("Initializing omap port scanner");
+    // unwrap to convert option enum to string
+    let address = matches.value_of("address").unwrap();
+    println!("Starting scan for address: {}", address);
+    scan();
+    println!("Stopping omap port scanner");
+}
+
+fn scan() {
     for port_number in 0..65535 {
         let addr: String = format!("192.168.0.177:{}",port_number);
         if let Ok(_stream) = TcpStream::connect(addr) {
             println!("Port open on: {}", port_number);
         }
     }
-    println!("Stopping omap port scanner");
 }
+
