fb89e1c5762f89a152948ca25f9fddea71b837f1
Omar Tsai <omar2535@gmail.com>
2020-03-26 22:03:18 -0700

    adding basic port scanning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Initializing omap port scanner");
+    for port_number in 0..65535 {
+        let addr: String = format!("192.168.0.177:{}",port_number);
+        if let Ok(_stream) = TcpStream::connect(addr) {
+            println!("Port open on: {}", port_number);
+        }
+    }
+    println!("Stopping omap port scanner");
 }
