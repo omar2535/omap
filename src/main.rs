cef1f36bfe5b7fc3ad535a55efb1e55e72de4cf1
Omar Tsai <omar2535@gmail.com>
2020-03-28 11:24:23 -0700

    separating matches into own function

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
 extern crate clap;
 
 use clap::{Arg, App};
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpStream;
 
 fn main() {
+
+    let matches = generate_cli_input();
+    println!("Initializing omap port scanner");
+    // unwrap to convert option enum to string
+    let address = matches.value_of("address").unwrap();
+    println!("Starting scan for address: {}", address);
+    scan(&address);
+    println!("Stopping omap port scanner");
+}
+
+fn generate_cli_input() -> clap::ArgMatches<'static> {
     let matches = App::new("Omap")
                           .version("1.0")
                           .author("Omar T. <omar2535@alumni.ubc.ca>")
                           .about("Scans open ports at specified address")
                           .arg(Arg::with_name("address")
-                              .short("-a")
-                              .long("address")
-                              .help("Sets the address to be scanned")
-                              .takes_value(true))
+                            .short("-a")
+                            .long("address")
+                            .help("Sets the address to be scanned")
+                            .required(true)
+                            .takes_value(true))
+                          .arg(Arg::with_name("v")
+                            .short("-v")
+                            .multiple(true)
+                            .help("Sets the level of verbosity"))
                           .get_matches();
-    println!("Initializing omap port scanner");
-    // unwrap to convert option enum to string
-    let address = matches.value_of("address").unwrap();
-    println!("Starting scan for address: {}", address);
-    scan();
-    println!("Stopping omap port scanner");
+    return matches;
 }
 
-fn scan() {
+fn scan(address: &str) {
     for port_number in 0..65535 {
-        let addr: String = format!("192.168.0.177:{}",port_number);
+        let addr: String = format!("{}:{}", address, port_number);
+        println!("Scanning on: {}", addr);
         if let Ok(_stream) = TcpStream::connect(addr) {
             println!("Port open on: {}", port_number);
         }
     }
 }
-
