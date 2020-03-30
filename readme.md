b25e885f79d5c9780d27496475470acca2dbf2a6
Omar Tsai <omar2535@gmail.com>
2020-03-29 23:41:24 -0700

    adding verbosity option

--- readme.md
+++ readme.md
@@ -1,13 +1,19 @@
-# omap port scanner
+# Omap port scanner
 
-This is a simple port scanner written in rust. Supports only IPV4 addresses for now.
+This is a simple port scanner written in rust. 
 
 ## Basic usage:
 
 ```sh
+Scans open ports at specified address
+
+USAGE:
+    omap [FLAGS] --address <address>
+
 FLAGS:
     -h, --help       Prints help information
     -V, --version    Prints version information
+    -v, --verbose    Sets the level of verbosity
 
 OPTIONS:
     -a, --address <address>    Sets the address to be scanned
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // unwrap to convert option enum to string
     let address = matches.value_of("address").unwrap();
     println!("Starting scan for address: {}", address);
-    scan(&address);
+    scan(&address, matches.is_present("verbose"));
     println!("Stopping omap port scanner");
 }
 
@@ -25,22 +25,22 @@ fn generate_cli_input() -> clap::ArgMatches<'static> {
                             .help("Sets the address to be scanned")
                             .required(true)
                             .takes_value(true))
-                          .arg(Arg::with_name("v")
+                          .arg(Arg::with_name("verbose")
                             .short("-v")
-                            .multiple(true)
+                            .long("verbose")
                             .help("Sets the level of verbosity"))
                           .get_matches();
     return matches;
 }
 
-fn scan(address: &str) {
+fn scan(address: &str, with_verbosity: bool) {
     for port_number in 0..65535 {
         let addr: String = format!("{}:{}", address, port_number);
         thread::spawn(move || {
-            // println!("Scanning on: {}", addr);
+            if with_verbosity {println!("Scanning on: {}", addr)};             
             if let Ok(_stream) = TcpStream::connect(addr) {
                 println!("Port open on: {}", port_number);
-            } 
+            }
         });
     }
 }
